d8fe004f7014d73c503729ddf80bf9e9e142e7e5
Vladimir Chernov <vlschernov@gmail.com>
2018-03-06 21:27:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tele2 Maxi LTE Device Tree for TWRP
+-----------------------------------
+
+Дерево для компиляции TWRP 3.0.2-0 на базе исходников OmniROM для Tele2 Maxi LTE.
+
---
+++ omni_w370.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/umi/w370/w370.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := w370
+PRODUCT_NAME := omni_w370
+PRODUCT_BRAND := UMI
+PRODUCT_MODEL := London
+PRODUCT_MANUFACTURER := UMI
